6ce342aa3ab87ba958f4ee349d371d4814debc20
Samuel-Ssenyonga <samuelssenyonga301@gmail.com>
2024-04-23 16:09:31 +0300

    Open Face

--- main_facial.py
+++ main_facial.py
@@ -1,14 +1,14 @@
 import cv2 as cv
-from picamera2 import PiCamera2
+from picamera2 import Picamera2
 # from picamera2 import PiCamera2 
 
 # Create a PiCamera2 object
-picam2 = PiCamera2()
+picam2 = Picamera2()
 
 # Configure preview settings
 picam2.preview_configuration.main.size = (800, 800)  # Corrected attribute name
-picam2.preview_configuration.main.format = "RGB"  # Changed format to "RGB" (no need for 888)
-picam2.preview_configuration.align = (0, 0)  # Corrected attribute name
+picam2.preview_configuration.main.format = "RGB888"  # Changed format to "RGB" (no need for 888)
+picam2.preview_configuration.align()
 
 # Start the preview
 picam2.configure("preview")  # Changed method call to correct syntax
